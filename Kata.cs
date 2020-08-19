4707977c155bc89c84ee85960cbf648ddad248ff
Karl <48979622+Karljh@users.noreply.github.com>
2020-08-19 21:10:32 +0200

    Find The Parity Outlier
    You are given an array (which will have a length of at least 3, but could be very large) containing integers. The array is either entirely comprised of odd integers or entirely comprised of even integers except for a single integer N. Write a method that takes the array as an argument and returns this "outlier" N.

---
+++ Kata.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection.Emit;
+using System.Text;
+
+namespace NUnitTestProject1
+{
+    class Kata
+    {
+        public static int Find(int[] integers)
+        {
+            var oddList = new List<int>();
+            var evenList = new List<int>();
+
+            foreach (var N in integers)
+            { 
+                if (N % 2 != 0)
+                {
+                    oddList.Add(N);
+                }
+                else
+                {
+                    evenList.Add(N);
+                }
+            }
+
+            if(oddList.Count > 1)
+            {
+                return evenList[0];
+            }
+
+            else
+            {
+                return oddList[0];
+            }
+        }
+    }
+}
